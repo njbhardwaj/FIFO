8a52f8ce420af0178a9235b1139ca3db26d1285e
Narayan Jee <128828098+njbhardwaj@users.noreply.github.com>
2025-03-06 14:28:43 +0530

    FIFObuffer.v

--- FIFObuffer.v
+++ FIFObuffer.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.11.2018 14:06:22
-// Design Name: 
-// Module Name: FIFObuffer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module FIFObuffer( Clk, 
 
                    dataIn, 
